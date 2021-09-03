126aaac70cbbdbd48bdb4520471032a56716ece4
Shivani R <shivanir.1901219@srec.ac.in>
2021-09-03 13:55:49 +0530

    Initial Commit

---
+++ Counter App/Counter_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Counter_AppApp.swift
+//  Counter App
+//
+//  Created by user on 03/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Counter_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
